6527c5d1f9a3292d347b6d1d2b88d560255649ba
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2023-05-16 14:07:13 -0300

    Add files via upload

---
+++ pyyyyy-main/pyyyyy-main/PRO_1-1_C108_AtividadeDaProfessora1-main/count_fingers.py
@@ -0,0 +1,58 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
+
+hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
+
+tipIds = [4, 8, 12, 16, 20]
+
+# Defina uma função para contar os dedos
+def countFingers(image, hand_landmarks, handNo=0):
+     # Desenhar as conexões entre os pontos de referência
+     if hand_landmarks:
+        # Obtenha todos os pontos de referência da PRIMEIRA mão VISÍVEL
+        landmarks = hand_landmarks[handNo].landmark
+        print(landmarks)
+     
+
+# Defina uma função para 
+def drawHandLanmarks(image, hand_landmarks):
+
+   # Desenhar as conexões entre os pontos de referência
+    if hand_landmarks:
+
+      for landmarks in hand_landmarks:
+               
+        mp_drawing.draw_landmarks(image, landmarks, mp_hands.HAND_CONNECTIONS)
+    
+while True:
+    success, image = cap.read()
+    
+    image = cv2.flip(image, 1)
+    
+    # Detecte os pontos de referência das mãos 
+    results = hands.process(image)
+    
+    # Obtenha a posição do ponto de referência do resultado processado
+    hand_landmarks = results.multi_hand_landmarks
+
+    # Desenhe os pontos de referência
+    drawHandLanmarks(image, hand_landmarks)
+    
+    # Obtenha a posição dos dedos da mão        
+    countFingers(image, hand_landmarks)
+
+    cv2.imshow("Controlador de Midia", image)
+
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+
+cv2.destroyAllWindows()
+
